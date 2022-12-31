b5d122dcc3499a0ca20d99e5912614666b292651
Rajkumarthakur88 <thakurraj8878@gmail.com>
2022-12-31 19:19:00 +0530

    toggle button added styled

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -3,6 +3,11 @@ import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 import { navlink } from "../data/dummydata"
 import logo from "../data/images/profile-img.jpg"
+import NightsStayIcon from '@mui/icons-material/NightsStay';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import ToggleOnIcon from '@mui/icons-material/ToggleOn';
+import ToggleOffIcon from '@mui/icons-material/ToggleOff';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 
 function Header() {
     const [responsive, setResponsive] = useState(false)
@@ -27,10 +32,13 @@ function Header() {
                 <div className='container flexsb nav-div'>
                     <div className='logo logo-div'>
                         <Link className='logo-div' to={"/"}> <img style={{ width: "50px", borderRadius: "50%" }} src={logo} alt='' data-aos='zoom-in-right' /></Link>
+                        <button className="toggle-btn"  onClick={() => handleMode()}>
+                            {
+                                theme === 'light-theme' ? <span style={{ display: "flex", fontSize: "55px" }}><FiberManualRecordIcon /> <NightsStayIcon /></span> : <span style={{ display: "flex", fontSize: "15px" }}> <LightModeIcon /><FiberManualRecordIcon /></span>
+                            }
+                        </button>
                     </div>
-                    <button className="toggle-btn" onClick={() => handleMode()}>
-                        Toggle
-                    </button>
+
                     <div className={responsive ? "hideMenu" : "nav"}>
                         {navlink.map((links, i) => (
                             <Link to={links.url} key={i} data-aos='zoom-in-left' className="nav-links">
